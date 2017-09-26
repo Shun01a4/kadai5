d432ea3d03ea25bb0bf2469748775b1acb861f18
Shun <j1223@PC4-54.cc.matsue-ct.jp>
2017-09-26 15:05:49 +0900

    enemy player update

--- main.rb
+++ main.rb
@@ -6,6 +6,8 @@ require_relative 'enemy'
 
 Window.width  = 800
 Window.height = 600
+loop_ct=0
+score=0
 
 player_img = Image.load("player.png")
 player_img.setColorKey([0, 0, 0])
@@ -15,20 +17,30 @@ enemy_img.setColorKey([0, 0, 0])
 
 player = Player.new(400, 500, player_img)
 
+font=Font.new(20)
+
 enemies = []
 10.times do
-  enemies << Enemy.new(rand(800), rand(600), enemy_img)
+  enemies << Enemy.new(rand(800), rand(300), enemy_img)
 end
 
 Window.loop do
   break if Input.keyPush?(K_ESCAPE)
+  if(loop_ct%3==0)
+ 	 enemies << Enemy.new(rand(800), rand(300), enemy_img)
+ 	 loop_ct=0
+  end
+  loop_ct+=1
 
   Sprite.update(enemies)
   Sprite.draw(enemies)
+  Window.draw_font(10,10,score.to_s,font)
 
   player.update
   player.draw
 
   # 当たり判定
-  Sprite.check(player, enemies)
+  if(Sprite.check(player, enemies))
+  	score+=1
+  end
 end
--- player.rb
+++ player.rb
@@ -1,8 +1,14 @@
 # coding: utf-8
 
 class Player < Sprite
+attr_accessor :x
+attr_accessor :y
   def update
-    self.x += Input.x
-    self.y += Input.y
+      self.x += Input.x*5
+      self.y += Input.y*5
+    if(self.x<0 || self.x>768 || self.y<0 || self.y>568)
+      self.x -= Input.x*5
+      self.y -= Input.y*5
+    end
   end
 end
