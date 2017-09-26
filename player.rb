67fa7c57252134a4e1f21431ef4ad9b54ad75693
Shun <j1223@PC4-54.cc.matsue-ct.jp>
2017-09-26 15:52:36 +0900

    player update

--- player.rb
+++ player.rb
@@ -1,14 +1,19 @@
 # coding: utf-8
 
 class Player < Sprite
-attr_accessor :x
-attr_accessor :y
   def update
-      self.x += Input.x*5
-      self.y += Input.y*5
-    if(self.x<0 || self.x>768 || self.y<0 || self.y>568)
-      self.x -= Input.x*5
-      self.y -= Input.y*5
-    end
+#    self.x += Input.x*5
+#    self.y += Input.y*5
+#    if(self.x<0 || self.x>768 || self.y<0 || self.y>568)
+#      self.x -= Input.x*5
+#      self.y -= Input.y*5
+#    end
+     self.x=Input.mouse_pos_x
+     self.y=Input.mouse_pos_y
+  end
+
+  def size(fat)
+  	self.scale_x=fat
+  	self.scale_y=fat
   end
 end
